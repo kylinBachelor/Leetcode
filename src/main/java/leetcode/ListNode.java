28912a653fec851222a4ba14a940d90a0426d2fe
kylinBachelor <kylinBachelor@outlook.com>
2024-12-03 11:00:20 +0800

    1

---
+++ src/main/java/leetcode/ListNode.java
@@ -0,0 +1,12 @@
+package main.java.leetcode;
+
+/**
+ * 链表结构
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    public ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
--- src/main/java/leetcode/leetcode2/Question.java
+++ src/main/java/leetcode/leetcode2/Question.java
@@ -1,5 +1,26 @@
 package main.java.leetcode.leetcode2;
 
+import java.util.Map;
+
+import main.java.leetcode.ListNode;
+
 public class Question {
+
+    public static void main(String[] args) {
+        System.out.println(10 >> 1);
+        
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result;
+
+
+
+
+
+
+
+        return  null;
+    }
     
 }
